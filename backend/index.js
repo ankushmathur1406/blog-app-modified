a36a0c0d9725535083b7b11b794250aa09143186
ankushmathur1406 <mathurankush1406@gmail.com>
2025-03-26 14:28:10 +0530

    origin change

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(cors({
     //origin:process.env.FRONTEND_URL,
    // origin:"http://localhost:5173",
-   origin:"https://blog-app-modified.vercel.app/",
+   origin:"https://blog-app-modified.vercel.app",
   // origin:"*",
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
