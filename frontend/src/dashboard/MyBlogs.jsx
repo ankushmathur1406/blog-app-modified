4dfa12e73e41755093797c19a7cfa3fcb5d2342b
ankushmathur1406 <mathurankush1406@gmail.com>
2025-03-26 14:41:48 +0530

    credentials

--- frontend/src/dashboard/MyBlogs.jsx
+++ frontend/src/dashboard/MyBlogs.jsx
@@ -11,7 +11,7 @@ function MyBlogs() {
         const { data } = await axios.get(
           //"http://localhost:4001/api/blogs/my-blog",
           "https://thoughtdrop.onrender.com/api/blogs/my-blog",
-          { withCredentials: true }
+         // { withCredentials: true }
         );
         console.log(data);
         setMyBlogs(data);
