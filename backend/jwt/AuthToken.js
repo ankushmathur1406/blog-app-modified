7bb5d32db81d3a72be60eb151b6e821bb20c0448
ankushmathur1406 <mathurankush1406@gmail.com>
2025-03-31 12:22:23 +0530

    change in auth func

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -5,18 +5,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const createTokenAndSaveCookies = async (userId, res) => {
-   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY,{
-    expiresIn: "30d",
-   });
-   res.cookie("jwt", token, {
-     httpOnly: false, // True se false kiye in production nhi krne par production me back aur front connect nhi hoga
-     secure: false,
-     sameSite: "none",// lax se none kiye
-    path: "/", // Ensure the cookie is available throughout the site
-   });
-  await User.findByIdAndUpdate(userId, {token});
-  return token;
-};
+// const createTokenAndSaveCookies = async (userId, res) => {
+//    const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY,{
+//     expiresIn: "30d",
+//    });
+//    res.cookie("jwt", token, {
+//      httpOnly: false, // True se false kiye in production nhi krne par production me back aur front connect nhi hoga
+//      secure: false,
+//      sameSite: "none",// lax se none kiye
+//     path: "/", // Ensure the cookie is available throughout the site
+//    });
+//   await User.findByIdAndUpdate(userId, {token});
+//   return token;
+// };
+
+
+
+function createTokenAndSaveCookies(userId){
+  return jwt.sign(
+    {userId},
+    process.env.JWT_SECRET_KEY
+  );
+}
 
  export default createTokenAndSaveCookies;
