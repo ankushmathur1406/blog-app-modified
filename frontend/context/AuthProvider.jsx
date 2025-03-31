1f1dd0d09d7e1856880397bab4705f4dc805390d
ankushmathur1406 <mathurankush1406@gmail.com>
2025-03-31 11:38:20 +0530

    changed auth.js

--- frontend/context/AuthProvider.jsx
+++ frontend/context/AuthProvider.jsx
@@ -37,8 +37,8 @@ export const AuthProvider = ({ children }) => {
     const fetchBlogs = async () => {
       try {
         const { data } = await axios.get(
-        //  "https://thoughtdrop.onrender.com/api/blogs/all-blogs",
-             "http://localhost:4001/api/blogs/all-blogs",
+         "https://thoughtdrop.onrender.com/api/blogs/all-blogs",
+         //    "http://localhost:4001/api/blogs/all-blogs",
           { withCredentials: true }
         );
         console.log(data);
